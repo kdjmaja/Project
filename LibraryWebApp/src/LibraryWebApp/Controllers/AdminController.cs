e716f43013e54d711957338a78f1b32f2ccbc8d1
galiuskas24 <galiuskas24@gmail.com>
2017-01-05 22:35:50 +0100

    Update AdminController.cs

--- LibraryWebApp/src/LibraryWebApp/Controllers/AdminController.cs
+++ LibraryWebApp/src/LibraryWebApp/Controllers/AdminController.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
-
+//promjena za probu
 namespace LibraryWebApp.Controllers
 {
     public class AdminController : Controller
