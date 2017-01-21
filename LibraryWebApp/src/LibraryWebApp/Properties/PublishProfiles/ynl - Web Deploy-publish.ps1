6ad7a47a39beff95f766bdf73a2832b850c75b6d
kdjmaja <maja.krmpotic-durdevic@fer.hr>
2017-01-21 23:47:33 +0100

    Final

---
+++ LibraryWebApp/src/LibraryWebApp/Properties/PublishProfiles/ynl - Web Deploy-publish.ps1
@@ -0,0 +1,19 @@
+﻿[cmdletbinding(SupportsShouldProcess=$true)]
+param($publishProperties=@{}, $packOutput, $pubProfilePath)
+
+# to learn more about this file visit https://go.microsoft.com/fwlink/?LinkId=524327
+
+try{
+    if ($publishProperties['ProjectGuid'] -eq $null){
+        $publishProperties['ProjectGuid'] = 'ae0d41a7-df82-4205-83e0-227e4427c60f'
+    }
+
+    $publishModulePath = Join-Path (Split-Path $MyInvocation.MyCommand.Path) 'publish-module.psm1'
+    Import-Module $publishModulePath -DisableNameChecking -Force
+
+    # call Publish-AspNet to perform the publish operation
+    Publish-AspNet -publishProperties $publishProperties -packOutput $packOutput -pubProfilePath $pubProfilePath
+}
+catch{
+    "An error occurred during publish.`n{0}" -f $_.Exception.Message | Write-Error
+}
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# Project
+# Project: Your Nearest Library
+
+https://ynl.azurewebsites.net/
+
+#Izradili:
+	Lea Rački
+	Vlado Galić
+	Maja Krmpotić Đurđević
+	
+#Podjela zadataka:
+	Lea: Google i Facebook login
+	Vlado: logo, osnovni kostur projekta (otprilike dio koji smo radili u 3. domaćoj zadaći)
+	Maja: autorizacija
+	zajedno: sve ostalo
+
+#Admin account
+	username: ferAdmin
+	password: survivors
+	
+#Mailman account
+	username: postar
+	password: postar
